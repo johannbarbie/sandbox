7cef8e29ef66e5c487dea883d99eb07dc6c52633
VonIobro <jonathan.zobro@gmail.com>
2014-08-23 16:08:02 -0700

    separation of responses from logic

--- myscripts.js
+++ myscripts.js
@@ -1,42 +1,58 @@
 $( document ).ready(function() {
-
 // Initial test
-window.onload = function() {
-    alert( 'Welcome to the 37coins SMS emulator' );
-}; // ( Initial test )
-
+window.onload = function() { alert( 'Welcome to the 37coins SMS emulator' ); }; // ( Initial test )
 
 // Click the 'Send' button
 $( 'button' ).click(function() {
-    alert( 'Handler for .click() called' );
+    parseCommand(); 
 });// ( Send button )
 
 // Hit 'enter' on form
 $( '#textBox' ).keydown(function(){
     if( event.which == 13 ){
-      alert( 'Handler for .keydown() called' ); 
-      registerDisplay; 
+      parseCommand();
     };
 });// ( Enter button )
 
-// Register and display messages
-(function registerDisplay() {
-
-    // Get the value from the input box
-    var value = $( 'input' ).val();
-    $( '.recordSent' ).text( value );
-
-    //whatResponse();
-    $( '.recordReceived' ).text( 'testing 2 and all that' );
-
-});// ( Register and display messages )
-
-
 });// ( document )
 
+/************************
+    Parsing of commands
+ ************************/
+// return different command depending on input
+function parseCommand(){
+    var inputValue = $( 'input' ).val();
 
+    // hello generates welcome message
+    if ( inputValue == 'hello' ){
+        var feedbackValue = welcomeMSG;
+    } else {
+        var feedbackValue = unknownMSG;
+    };
 
+    displayFeedback(inputValue, feedbackValue);
+}// ( parseCommand )
 
+/**********************
+    Display Commands
+***********************/
+// Register and display messages
+function displayFeedback(inputValue, feedbackValue) {
+
+    // Get the value from the input box
+    $( '.recordSent' ).text( inputValue );
+
+    //whatResponse();
+    $( '.recordReceived' ).text( feedbackValue );
+
+}// ( Register and display messages )
+
+/******************
+    SMS responses
+********************/
+var welcomeMSG = 'welcome';
+var unknownMSG = 'unknown command, please try again';
+// ( SMS responses )
 
 
 
